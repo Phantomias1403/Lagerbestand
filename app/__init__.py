02d4c0a3f39c985cee981e0d180c98b6f93adf71
Phantomias1403 <finn.baumann14@gmail.com>
2025-06-27 00:15:45 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,41 +3,35 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-# Datenbank und LoginManager global definieren
 db = SQLAlchemy()
 login_manager = LoginManager()
 
-
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'change-me'  # für Sessions/Login
+    app.config['SECRET_KEY'] = 'change-me'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
 
-    # Benutzerverwaltung aktivieren: über Umgebungsvariable oder dauerhaft
-    app.config['ENABLE_USER_MANAGEMENT'] = True
-    # Falls du das immer aktiv haben willst, ersetze die Zeile durch:
-    # app.config['ENABLE_USER_MANAGEMENT'] = True
+    # Benutzerverwaltung aktivieren über Umgebungsvariable ENABLE_USER_MANAGEMENT (default = aktiviert)
+    app.config['ENABLE_USER_MANAGEMENT'] = os.environ.get('ENABLE_USER_MANAGEMENT', '1') == '1'
 
-    # Initialisiere Erweiterungen
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'main.login'
 
-    # Kontextvariable für Template (z. B. enable_user_management im Menü)
+    # Template-Variablen bereitstellen
     @app.context_processor
     def inject_config():
-        return dict(enable_user_management=app.config.get['ENABLE_USER_MANAGEMENT'])
+        return dict(enable_user_management=app.config['ENABLE_USER_MANAGEMENT'])
 
-    # Registriere Blueprint & lade Modelle
     with app.app_context():
         from . import routes, models
         app.register_blueprint(routes.bp)
         db.create_all()
 
-        # Admin-Nutzer automatisch anlegen, falls keine Benutzer existieren
+        # Nur Admin-Nutzer anlegen, wenn Benutzerverwaltung aktiv ist
         if app.config['ENABLE_USER_MANAGEMENT'] and models.User.query.count() == 0:
             admin = models.User(username='admin', is_admin=True)
-            admin.set_password('admin')  # Passwort: admin
+            admin.set_password('admin')
             db.session.add(admin)
             db.session.commit()
 
