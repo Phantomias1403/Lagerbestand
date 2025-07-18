f3706c2833c2bd21ba20e653988e4981b82e78f6
Phantomias1403 <finn.baumann14@gmail.com>
2025-07-18 14:43:45 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'main.login'
+    login_manager.login_view = 'main.select_profile'
 
     # Template-Variablen bereitstellen
     @app.context_processor
