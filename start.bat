746f42e1e0c1373f81d1c9f8a2787c5022d050ef
Phantomias1403 <finn.baumann14@gmail.com>
2025-07-18 14:24:32 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,7 +4,7 @@ cd /d %~dp0
 echo Starte das Lagersystem...
 
 :: Öffnet automatisch den Browser
-start http://127.0.0.1:5000
+start http://192.168.178.93:5000
 
 :: Startet den Python-Server
 python run.py
