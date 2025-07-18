b680b5012b75f162b89a400c2d4ab651284f2cd1
Phantomias1403 <finn.baumann14@gmail.com>
2025-07-18 16:27:57 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     is_staff = db.Column(db.Boolean, default=False)
