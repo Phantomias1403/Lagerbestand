0ef09ed339c097b3efefded0ec80b2bcf2edf057
Phantomias1403 <finn.baumann14@gmail.com>
2025-10-16 13:41:45 +0200

    Update apps.py

--- lagerbestand_site/core/apps.py
+++ lagerbestand_site/core/apps.py
@@ -3,5 +3,6 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lagerbestand_site.core'
+    name = 'core'
+    label = 'core'
     verbose_name = 'Lagerverwaltung'
